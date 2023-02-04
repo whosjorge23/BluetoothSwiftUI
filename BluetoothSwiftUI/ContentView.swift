e16f6e2b806c0e7e8d188626ae4cf816eb0fc56f
Giorgio Giannotta <jorge_upa@msn.com>
2023-02-04 17:35:31 +0100

    Set up the scanner for Devices

--- BluetoothSwiftUI/ContentView.swift
+++ BluetoothSwiftUI/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+
+class BluetoothScanner: NSObject, ObservableObject{
+    private var centralManager: CBCentralManager?
+    private var peripherals: [CBPeripheral] = []
+    @Published var peripharalNames: [String] = []
+    
+    override init() {
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: .main)
+    }
+}
+
+extension BluetoothScanner : CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            self.centralManager?.scanForPeripherals(withServices: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral) {
+            self.peripherals.append(peripheral)
+            self.peripharalNames.append(peripheral.name ?? "Unnamed Device")
+        }
+    }
+    
+}
 
 struct ContentView: View {
+    @ObservedObject private var scanner = BluetoothScanner()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Bluetooth Devices")
+                .font(.largeTitle)
+                .padding()
+            NavigationView {
+                List(scanner.peripharalNames, id: \.self) { device in
+                    Text(device)
+                }
+                .navigationTitle("Devices")
+            }
+            
         }
-        .padding()
     }
 }
 
