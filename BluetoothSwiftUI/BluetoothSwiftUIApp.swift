4e918867cf1faeab3bf7cdeeca62d447ac19a621
Giorgio Giannotta <jorge_upa@msn.com>
2023-02-04 16:02:03 +0100

    Initial Commit

---
+++ BluetoothSwiftUI/BluetoothSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothSwiftUIApp.swift
+//  BluetoothSwiftUI
+//
+//  Created by Giorgio Giannotta on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
